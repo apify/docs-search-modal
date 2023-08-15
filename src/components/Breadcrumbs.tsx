9a809bd25b69be240e14e2e63bc2549ce1c459fd
Jindřich Bär <jindrichbar@gmail.com>
2023-08-15 11:13:17 +0200

    feat (visual): don't render empty breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -9,11 +9,11 @@ export function Breadcrumbs({ item, highlight }: any) {
     return (
       <div className='overflow-x-hidden overflow-ellipsis whitespace-nowrap overflow-y-hidden h-auto'>
         { 
-          x.map(([_,b]: any, index) => ( b?.value &&
-          <span style={{color: 'slategray'}} key={index.toString()}>
+          x.map(([_,b]: any, index) => ( (b?.value && b.value.length > 3) ?
+          (<span style={{color: 'slategray'}} key={index.toString()}>
             {highlight({hit: { value: b.value ? decode(b.value) : '' }, attribute: 'value'})}
             {index < x.length - 1 && <span className="breadcrumb-separator" style={{color: '#bbb'}}> &gt; </span>}
-          </span>
+          </span>) : null
         )).filter(x => x)
         }
       </div>
