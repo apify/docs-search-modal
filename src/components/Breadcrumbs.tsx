42639d29c25d773d4ff0b91a30bf9c1e0ad26242
Jindřich Bär <jindrichbar@gmail.com>
2023-08-16 10:08:07 +0200

    fix: breadcrumbs rendering (separators, line-height)

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -7,12 +7,11 @@ export function Breadcrumbs({ item, highlight }: any) {
     const x = Object.entries(item?._highlightResult?.hierarchy ?? {}).filter(([_,b]) => b).sort(([a],[b]) => a.localeCompare(b));
   
     return (
-      <div className='overflow-x-hidden overflow-ellipsis whitespace-nowrap overflow-y-hidden h-auto'>
+      <div className='overflow-x-hidden overflow-ellipsis whitespace-nowrap leading-normal h-auto'>
         { 
           x.map(([_,b]: any, index) => ( (b?.value && b.value.length > 3) ?
-          (<span style={{color: 'slategray'}} key={index.toString()}>
+          (<span style={{color: 'slategray'}} className="breadcrumbs__item" key={index.toString()}>
             {highlight({hit: { value: b.value ? decode(b.value) : '' }, attribute: 'value'})}
-            {index < x.length - 1 && <span className="breadcrumb-separator" style={{color: '#bbb'}}> &gt; </span>}
           </span>) : null
         )).filter(x => x)
         }
