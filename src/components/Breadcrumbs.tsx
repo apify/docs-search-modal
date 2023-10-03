4de496fa0f84e6f40087627e0146d57a51989c44
Jindřich Bär <jindrichbar@gmail.com>
2023-10-03 13:32:00 +0200

    fix: remove last breadcrumb item

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -4,15 +4,16 @@ import { decode } from "html-entities";
  * Renders breadcrumbs for the current item.
  */
 export function Breadcrumbs({ item, highlight }: any) {
-    const x = Object.entries(item?._highlightResult?.hierarchy ?? {})
+    let breadcrumbs = Object.entries(item?._highlightResult?.hierarchy ?? {})
       .filter(([_,b]) => b)
       .sort(([a],[b]) => a.localeCompare(b))
       .slice(1);
-  
+
+    if(breadcrumbs.length > 1) breadcrumbs = breadcrumbs.slice(0, -1);
     return (
       <div className='overflow-x-hidden overflow-ellipsis whitespace-nowrap leading-normal h-auto'>
         { 
-          x.map(([_,b]: any, index) => ( (b?.value && b.value.length > 3) ?
+          breadcrumbs.map(([_,b]: any, index) => ( (b?.value && b.value.length > 3) ?
           (<span style={{color: 'slategray'}} className="breadcrumbs__item" key={index.toString()}>
             {highlight({hit: { value: b.value ? decode(b.value) : '' }, attribute: 'value'})}
           </span>) : null
