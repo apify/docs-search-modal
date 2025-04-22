46bce7a0ee381a8d76166e5ad3951bfa00fff7d3
Jindřich Bär <jindrichbar@gmail.com>
2025-04-22 12:45:36 +0200

    feat: use code highlighter from `@apify/ui-library` (#14)
    Switches from using the `react-syntax-highlighter` library directly to using the `CodeBlock` component from `@apify/ui-library`.
    
    This allows us to solve the dependabot issue with outdated `prismjs` only once and for all (in `@apify/ui-library`).

---
+++ src/components/Modal.tsx
@@ -0,0 +1,44 @@
+import { UiDependencyProvider } from "@apify/ui-library";
+import { Footer } from "./Footer";
+import { PreviewPanel } from "./PreviewPanel";
+import { ResultsItems } from "./ResultsItems";
+
+function getWarningMessage(methodName: string) {
+    return `WATCH OUT! ${methodName} prop in @apify/docs-search-modal is not correctly implemented!
+If you're seeing this message, it means that you're using components that are trying to access
+${methodName}, but the prop is not provided. This is a warning!`;
+    
+}
+
+export function Modal({state, setActiveItemId, setContext, components, preview}: { state: any, setActiveItemId: any, setContext: any, components: any, preview: any }) {
+    return (
+        <UiDependencyProvider
+            dependencies={
+                {
+                    InternalImage: (() => { console.warn(getWarningMessage('InternalImage')); return null; }) as any,
+                    InternalLink: (() => { console.warn(getWarningMessage('InternalLink')); return null; }) as any,
+                    isHrefTrusted: (() => {console.warn(getWarningMessage('isHrefTrusted')); return true}) as any,
+                    windowLocationHost: window.location.host,
+                    uiTheme: document.querySelector('[data-theme="dark"]') ? 'DARK' : 'LIGHT',
+                }
+            }
+            > <div className="flex flex-col h-full bg-white dark:bg-slate-800">
+        {state.collections[0].items.length === 0 ? (
+        <div className="flex flex-col justify-center items-center flex-1">
+            <div className='text-slate-400 font-medium text-lg px-3 py-1'>
+            No results :(
+            </div>
+        </div>
+        ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-1 flex-1 overflow-hidden">
+            <div className="h-full overflow-y-scroll resultsItems">
+            <ResultsItems {...{ items: state?.collections?.[0]?.items, setActiveItemId, setContext, state, components }} />
+            </div>
+            <PreviewPanel preview={preview} components={components} />
+        </div>
+        )}
+        <Footer />
+    </div>
+    </UiDependencyProvider>
+  );
+}
