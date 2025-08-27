bfd839bbf712d9b16489f3eb968bc3970d9a0c5e
Jindřich Bär <jindrichbar@gmail.com>
2025-08-27 16:18:40 +0200

    chore: remove old logo from search modal footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,7 +32,7 @@ function Tooltip() {
     </ul>
     )
   }
-  
+
   /**
   * Renders the footer of the modal with the Apify logo.
   */
@@ -41,16 +41,7 @@ function Tooltip() {
       <div className='hidden lg:block bg-white dark:bg-slate-900'>
       <div className="flex flex-row justify-between h-16 w-full items-center px-5 shadow-inner">
         <Tooltip />
-        <img 
-          style={{ height: '24px' }} 
-          alt="Apify logo" 
-          src={
-            document.querySelector('[data-theme="dark"') ? 
-            "https://cdn.hashnode.com/res/hashnode/image/upload/v1668430709262/I6SM9Q_Ej.png" :
-            "https://upload.wikimedia.org/wikipedia/commons/2/28/Apify-logo.svg"
-          }
-        />
-    </div>
+      </div>
     </div>
     );
   }
