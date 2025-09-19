31444c9155a1bf2784e3c2817e7792ee012ecd27
Jindřich Bär <jindrichbar@gmail.com>
2025-09-19 11:07:30 +0200

    feat: move Ask AI button to the search bar

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,7 +11,7 @@ ${methodName}, but the prop is not provided. This is a warning!`;
 
 }
 
-function AskAITab({ currentQuery, setOpen }: { currentQuery?: any, setOpen: (open: boolean) => void }) {
+export function AskAITab({ currentQuery, setOpen }: { currentQuery?: any, setOpen: (open: boolean) => void }) {
     const [isKapaAIAvailable, setKapaAIAvailable] = useState(true);
 
     useEffect(() => {
@@ -29,14 +29,14 @@ function AskAITab({ currentQuery, setOpen }: { currentQuery?: any, setOpen: (ope
             !isKapaAIAvailable ? null :
             <button
                 onClick={askKapaAI}
-                style={{ padding: '10px', backgroundColor: 'rgb(234,88,12)', cursor: 'pointer' }}
-                className="absolute top-0 right-3 dark:bg-slate-700 text-white dark:text-white font-medium rounded-b-lg shadow-md hover:bg-orange-400 transition">
+                style={{ padding: '7px', backgroundColor: 'rgb(234,88,12)', cursor: 'pointer', zIndex: 10000 }}
+                className="absolute text-white dark:text-white font-medium rounded-md shadow-lg transition kapa-ai-button bg-orange-500">
                 ✨ Ask AI
             </button>
     );
 }
 
-export function Modal({state, setActiveItemId, setContext, components, preview, setOpen}: { state: any, setActiveItemId: any, setContext: any, components: any, preview: any, setOpen: (open: boolean) => void }) {
+export function Modal({state, setActiveItemId, setContext, components, preview}: { state: any, setActiveItemId: any, setContext: any, components: any, preview: any }) {
     return (
         <UiDependencyProvider
             dependencies={
@@ -49,10 +49,6 @@ export function Modal({state, setActiveItemId, setContext, components, preview, 
                 }
             }
             > <div className="flex flex-col h-full bg-white dark:bg-slate-800 relative">
-            <AskAITab
-                currentQuery={state.query}
-                setOpen={setOpen}
-            />
         {state.collections[0].items.length === 0 ? (
         <div className="flex flex-col justify-center items-center flex-1">
             <div className='text-slate-400 font-medium text-lg px-3 py-1'>
