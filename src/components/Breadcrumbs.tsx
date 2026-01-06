87baca30bd3731ee6cffdd0d402d7417d4f5962d
Jindřich Bär <jindrichbar@gmail.com>
2026-01-06 09:31:40 +0100

    feat: render API paths as code snippets in breadcrumbs (#26)

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -6,7 +6,7 @@ import { isHeading } from "../utils/isHeading";
  */
 export function Breadcrumbs({ item, highlight }: any) {
     const breadcrumbs = [
-      ...((!isHeading(item)) ? 
+      ...((!isHeading(item)) ?
         item?.breadcrumbs.map((x: any) => [null,{ value: x, matchLevel: 'full' }] ?? []) :
         item?.breadcrumbs.map((x: any) => [null,{ value: x, matchLevel: 'full' }] ?? []).slice(-1)
       ),
@@ -15,15 +15,27 @@ export function Breadcrumbs({ item, highlight }: any) {
         .sort(([a],[b]) => a.localeCompare(b))
         .slice(1)
     ].slice(1);
-  
+
     return (
       <div className='overflow-x-hidden overflow-ellipsis whitespace-nowrap leading-normal h-auto'>
-        { 
-          breadcrumbs.map(([_,b]: any, index) => ( (b?.value && b.value.length > 3) ?
-          (<span style={{color: 'slategray'}} className="breadcrumbs__item" key={index.toString()}>
-            {highlight({hit: { value: b.value ? decode(b.value) : '' }, attribute: 'value'})}
-          </span>) : null
-        )).filter(x => x)
+        {
+          breadcrumbs.map(([_,b]: any, index) => {
+          if (!b?.value || b.value.length < 2) {
+            return null;
+          }
+
+          return (<span style={{color: 'slategray'}} className="breadcrumbs__item" key={index.toString()}>
+            {
+              decode(b.value).startsWith('/v') ?
+              (<code className="bg-gray-100 dark:bg-slate-900 rounded-md px-1">
+                {highlight({hit: { value: b.value ? decode(b.value) : '' }, attribute: 'value'})}
+              </code>) :
+              (<span>
+                {highlight({hit: { value: b.value ? decode(b.value) : '' }, attribute: 'value'})}
+              </span>)
+            }
+          </span>)
+        }).filter(x => x)
         }
       </div>
     )
